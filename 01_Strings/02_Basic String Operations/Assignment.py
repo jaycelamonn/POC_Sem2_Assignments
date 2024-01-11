9cea9ddeaea710887bd4771178ee1280e7bdf94d
Jayce Moore <moorejay003@warren.k12.in.us>
2024-01-11 10:02:31 -0500

    assignment.py

--- 01_Strings/02_Basic String Operations/Assignment.py
+++ 01_Strings/02_Basic String Operations/Assignment.py
@@ -1,4 +1,4 @@
+bit_of_text = input("Hello")
 
-text1 = input("Enter some text: ")
-text2 = input("Enter some more text: ")
-print(text1 ==text2 )
+for char in bit_of_text:
+    print(ord(char))
---
+++ 01_Strings/03_String Comparisons/Assignment.py
@@ -0,0 +1,5 @@
+
+text1 = input("Enter some text: ")
+text2 = input("Enter some more text: ")
+print(text1 ==text2 )
+
