599725d150bdb0d1fcbb15344a82c1c1d8957a55
Jayce Moore <moorejay003@warren.k12.in.us>
2024-02-07 09:42:44 -0500

    assignment.py

--- 06_Types, Modules, and Packages/01_Type Annotations/assignment.py
+++ 06_Types, Modules, and Packages/01_Type Annotations/assignment.py
@@ -1,9 +1,9 @@
 #add type annotation on the functions and variables
 #variables types are float, function return type is a float
 
-def average(a, b):
+def average(a : float, b) -> float:
     return (a + b) / 2
 
-number1 = float(input("Enter a decimal number: "))
+number1 : float = float(input("Enter a decimal number: "))
 number2 = float(input("Enter another decimal number: "))
 print(average(number1, number2))
---
+++ 06_Types, Modules, and Packages/Lesson 6.3/assignment.py
@@ -0,0 +1,9 @@
+
+
+import random
+
+dice_rolls = [1, 2, 3, 4, 5, 6]
+
+random_roll =  random.choice(dice_rolls)
+
+print("You rolled a", random_roll)
