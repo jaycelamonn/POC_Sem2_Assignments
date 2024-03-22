992085102a42e8b92f74d4ca6e9b69f7370cba95
Jayce Moore <moorejay003@warren.k12.in.us>
2024-03-22 09:50:30 -0400

    assignment.py

--- 10_Functional Programming/03_File Handling/assignment.py
+++ 10_Functional Programming/03_File Handling/assignment.py
@@ -1,10 +1,10 @@
-file_path = "C:\\Users\\Nicholas Armstrong\\Development\\POC_Sem2_Assignments\\10_Functional Programming\\03_File Handling\\assignment.py"
+file_path = "C:\\Users\\moorejay003\\POC_Sem2_Assignments\\10_Functional Programming\\03_File Handling\\myFile.txt"
 
 try:
     stream = open(file_path)
-    #YOUDO print to console using stream.read()
-    #YOUDO don't forget to close the stream
-except:
-    pass
-##YOUDO rest of the program for reading and printing to the console
-##remember pass in file_path to open 
+    print(stream.read())
+    stream.close()
+  
+    
+except Exception as e:
+    print('An error occurred: ', e)
--- 10_Functional Programming/03_File_Handling_2/assignment.py
+++
@@ -1,1 +0,0 @@
-file_path = "C:\\Users\\Nicholas Armstrong\\Development\\POC_Sem2_Assignments\\10_Functional Programming\\03_File_Handling_2\\newfile.txt"
