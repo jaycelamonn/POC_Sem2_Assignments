0b026dc11389dc74d960ba7d4ff045de7e701dc4
Jayce Moore <moorejay003@warren.k12.in.us>
2024-03-22 10:01:04 -0400

    assignment.py

--- 10_Functional Programming/03_File Handling/assignment.py
+++ 10_Functional Programming/03_File Handling/assignment.py
@@ -1,8 +1,14 @@
 file_path = "C:\\Users\\moorejay003\\POC_Sem2_Assignments\\10_Functional Programming\\03_File Handling\\myFile.txt"
 
 try:
-    stream = open(file_path)
+    stream = open(
+        "C:\\Users\\moorejay003\\POC_Sem2_Assignments\\10_Functional Programming\\03_File Handling\\myFile.txt")
+
+    
     print(stream.read())
+    stream.write('This is your file')
+    stream.write('Run this to see if its your file!')
+    stream.write('This is still your file')
     stream.close()
   
     
